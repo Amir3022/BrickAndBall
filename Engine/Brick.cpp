3d9d8f119baebc2fcb8a74607879d69c3d9b4b40
Amir Tayie <amir.tayie@hotmail.com>
2022-09-13 00:16:16 +0200

    Added side collision detection to the ball class

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -26,8 +26,28 @@ void Brick::ExecuteCollideWithBall(Ball& ball, float dt)
 	assert(ball.GetRect().IsOverlappigWith(rect));
 	if (!bDestroyed)
 	{
+		Vec2 ballVel = ball.getVel();
 		Rect ballRect = ball.GetRect();
-		ball.ReboundY();
+		if (rect.right - ballRect.left <= -ballVel.x * dt)
+		{
+			ball.addPos(Vec2(ballVel.x * dt, 0.0f));
+			ball.ReboundX();
+		}
+		else if (ballRect.right - rect.left <= ballVel.x * dt)
+		{
+			ball.addPos(Vec2( -ballVel.x * dt, 0.0f));
+			ball.ReboundX();
+		}
+		if (ballRect.bottom - rect.top <= ballVel.y * dt)
+		{
+			ball.addPos(Vec2(0.0f, -ballVel.y * dt));
+			ball.ReboundY();
+		}
+		else if (ballRect.top - rect.bottom >= ballVel.y * dt)
+		{
+			ball.addPos(Vec2(0.0f, ballVel.y * dt));
+			ball.ReboundY();
+		}
 		bDestroyed = true;
 	}
 }
