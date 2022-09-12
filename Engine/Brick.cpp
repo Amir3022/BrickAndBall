d89efdcb8d315438f13fd7496c54aeb3f59b4d76
Amir Tayie <amir.tayie@hotmail.com>
2022-09-13 00:25:53 +0200

    Fixed check collision function in Brick class

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -18,7 +18,11 @@ void Brick::Draw(Graphics& gfx)
 
 bool Brick::CheckCollideWithBall(const Ball& ball) const
 {
-	return ball.GetRect().IsOverlappigWith(rect);
+	if (!bDestroyed)
+	{
+		return ball.GetRect().IsOverlappigWith(rect);
+	}
+	return false;
 }
 
 void Brick::ExecuteCollideWithBall(Ball& ball, float dt)
@@ -30,22 +34,22 @@ void Brick::ExecuteCollideWithBall(Ball& ball, float dt)
 		Rect ballRect = ball.GetRect();
 		if (rect.right - ballRect.left <= -ballVel.x * dt)
 		{
-			ball.addPos(Vec2(ballVel.x * dt, 0.0f));
+			//ball.addPos(Vec2(ballVel.x * dt, 0.0f));
 			ball.ReboundX();
 		}
 		else if (ballRect.right - rect.left <= ballVel.x * dt)
 		{
-			ball.addPos(Vec2( -ballVel.x * dt, 0.0f));
+			//ball.addPos(Vec2( -ballVel.x * dt, 0.0f));
 			ball.ReboundX();
 		}
 		if (ballRect.bottom - rect.top <= ballVel.y * dt)
 		{
-			ball.addPos(Vec2(0.0f, -ballVel.y * dt));
+			//ball.addPos(Vec2(0.0f, -ballVel.y * dt));
 			ball.ReboundY();
 		}
 		else if (ballRect.top - rect.bottom >= ballVel.y * dt)
 		{
-			ball.addPos(Vec2(0.0f, ballVel.y * dt));
+			//ball.addPos(Vec2(0.0f, ballVel.y * dt));
 			ball.ReboundY();
 		}
 		bDestroyed = true;
